687bc48be99e72b10a49665f0f1a3b8cc596f691
Gajanan Patil <patil.gajananps@gmail.com>
2017-12-07 01:17:05 +0530

    .

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res) => {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/speedops-sonar', (req, res) => {
+  console.log(req.body)
   res.status(200).send("I'm alive!")
 })
 
